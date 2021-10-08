e5e2d2cf1dfafe71e376ac96b9d0a2f97254015e
Alexey Pashinov <pashinov93@gmail.com>
2021-10-08 13:10:55 +0300

    Add utility to migrate old addresses into a new ton-wallet-api

---
+++ src/main.rs
@@ -0,0 +1,169 @@
+use std::convert::TryInto;
+use std::env;
+use std::fs::File;
+use std::path::PathBuf;
+
+use anyhow::{Context, Error, Result};
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Argon2,
+};
+use chacha20poly1305::aead::AeadMut;
+use chacha20poly1305::{ChaCha20Poly1305, Nonce};
+use clap::{AppSettings, Clap};
+use csv::Reader;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use sqlx::types::Uuid;
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Options {
+    #[clap(short, long)]
+    input: PathBuf,
+    #[clap(short, long)]
+    key: String,
+    #[clap(short, long)]
+    salt: Option<String>,
+    #[clap(short, long)]
+    with_headers: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    let args = Options::parse();
+    let Options {
+        input,
+        key,
+        salt,
+        with_headers,
+    } = args;
+    let key_string = key;
+    let reader = csv::ReaderBuilder::new()
+        .has_headers(with_headers)
+        .from_path(input)
+        .context("Couldn't read input file")?;
+
+    let database_url =
+        env::var("DATABASE_URL").context("The DATABASE_URL environment variable must be set")?;
+    let db_pool_size = env::var("DATABASE_POOL")
+        .context("The DATABASE_POOL environment variable must be set")?
+        .parse::<u32>()
+        .context("failed to get pg pool")?;
+
+    let pool = PgPoolOptions::new()
+        .max_connections(db_pool_size)
+        .connect(&database_url)
+        .await
+        .context("fail pg pool")?;
+
+    let sqlx_client = SqlxClient::new(pool);
+
+    let salt = match salt {
+        Some(salt) => SaltString::new(&salt).map_err(Error::msg)?,
+        None => SaltString::generate(&mut OsRng),
+    };
+    println!("Password salt: {}", salt.as_str());
+
+    let mut options = argon2::ParamsBuilder::default();
+    let options = options
+        .output_len(32) //chacha key size
+        .and_then(|x| x.clone().params())
+        .map_err(Error::msg)?;
+
+    // Argon2 with default params (Argon2id v19)
+    let argon2 = Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, options);
+
+    let key = argon2
+        .hash_password(key_string.as_bytes(), &salt)
+        .map_err(Error::msg)?
+        .hash
+        .context("No hash")?
+        .as_bytes()
+        .try_into()?;
+    run(sqlx_client, reader, key).await
+}
+
+async fn run(sqlx_client: SqlxClient, mut reader: Reader<File>, key: [u8; 32]) -> Result<()> {
+    let mut count = 0;
+    let iter = reader.deserialize();
+    for result in iter {
+        let record: AddressDb = result.context("Failed mapping to AddressDb")?;
+
+        let id = sqlx_client.get_id(record.workchain_id, &record.hex).await?;
+        let private_key = encrypt(&record.private_key, key, &id)?;
+
+        sqlx_client
+            .update_private_key(record.workchain_id, &record.hex, &private_key)
+            .await?;
+
+        count += 1;
+        println!("{}. {}:{} updated", count, record.workchain_id, record.hex)
+    }
+
+    Ok(())
+}
+
+fn encrypt(private_key: &str, key: [u8; 32], id: &uuid::Uuid) -> Result<String> {
+    use chacha20poly1305::aead::NewAead;
+    let nonce = Nonce::from_slice(&id.as_bytes()[0..12]);
+    let key = chacha20poly1305::Key::from_slice(&key[..]);
+    let mut encryptor = ChaCha20Poly1305::new(key);
+    let res = encryptor
+        .encrypt(nonce, base64::decode(&private_key)?.as_slice())
+        .unwrap();
+
+    Ok(base64::encode(res))
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, Eq, PartialEq)]
+pub struct AddressDb {
+    pub workchain_id: i32,
+    pub hex: String,
+    pub private_key: String,
+}
+
+#[derive(Clone)]
+pub struct SqlxClient {
+    pool: PgPool,
+}
+
+impl SqlxClient {
+    pub fn new(pool: PgPool) -> SqlxClient {
+        SqlxClient { pool }
+    }
+
+    pub async fn get_id(&self, workchain_id: i32, hex: &str) -> Result<Uuid> {
+        sqlx::query!(
+            r#"SELECT id
+                FROM address
+                WHERE workchain_id = $1 AND hex = $2"#,
+            workchain_id,
+            hex,
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_err(From::from)
+        .map(|x| x.id)
+    }
+
+    pub async fn update_private_key(
+        &self,
+        workchain_id: i32,
+        hex: &str,
+        private_key: &str,
+    ) -> Result<()> {
+        let _address = sqlx::query!(
+            r#"UPDATE address SET private_key = $1
+            WHERE workchain_id = $2 AND hex = $3"#,
+            private_key,
+            workchain_id,
+            hex,
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+}
